e3a9e30c9167c8ed89a85b653452daaa00855c58
zhizhi-cym <chenyinmeng54@gmail.com>
2026-08-28 22:09:45 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/my-first-app-chinese-medical/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
